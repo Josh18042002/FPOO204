947c7e7a991e11ea8251d79e8a8c61f482287d60
Josh18042002 <Taurojoca789@gmail.com>
2025-01-22 14:43:29 -0600

    Estructura de un archivo java y Menjo de cadenas

--- Sintaxis/src/Main.java
+++ Sintaxis/src/Main.java
@@ -1,11 +1,27 @@
+
+//IMPORTACIONES
+
+//Clase Principal
 public class Main {
 
+    //Metodo Main
+
+    /* soy
+    * un  comentarios
+    * de varias Lineas */
     public static void main(String[] args) {
 
 
+        System.out.println("Hello, Joshua Moises!");
+        String cadenas = "Joshua" + "Moises" + "Cervantes";
 
-        System.out.println("Hello, World!");
+        System.out.println("Cadenas");
+        System.out.println(cadenas.length());
 
-        
+        System.out.println(cadenas.substring(2,5));
+        System.out.println(cadenas.substring(2));
+        System.out.println(cadenas.substring(0,5));
+
+
     }
 }
