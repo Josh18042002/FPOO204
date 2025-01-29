7bff0229a9dc4d8f7187d25218f742745c6f89d7
Josh18042002 <Taurojoca789@gmail.com>
2025-01-28 18:11:06 -0600

    Ejercicio_4

--- Condicionales/src/Ejercicio_4.java
+++ Condicionales/src/Ejercicio_4.java
@@ -1,7 +1,27 @@
 import java.util.Scanner;
 
 public class Ejercicio_4 {
-     public static void main(String[] args) {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
 
-     }
+        System.out.print("Por favor, ingresa una cadena de caracteres: ");
+        String cadena = scanner.nextLine();
+
+        if (esPalindromo(cadena)) {
+            System.out.println("La cadena '" + cadena + "' es un palíndromo.");
+        } else {
+            System.out.println("La cadena '" + cadena + "' no es un palíndromo.");
+        }
+
+        scanner.close();
+    }
+
+    public static boolean esPalindromo(String cadena) {
+        // Elimina espacios y convierte a minúsculas para comparar
+        cadena = cadena.replaceAll("\\s", "").toLowerCase();
+
+        // Compara la cadena con su reverso
+        String reverso = new StringBuilder(cadena).reverse().toString();
+        return cadena.equals(reverso);
+    }
 }
