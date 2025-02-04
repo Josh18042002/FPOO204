61449ca1dfb41d68b81b6c90c25bf32b72a52e13
Josh18042002 <Taurojoca789@gmail.com>
2025-02-04 17:47:59 -0600

    multiplicador

--- Examen_1P/src/Ejercicio_1.java
+++ Examen_1P/src/Ejercicio_1.java
@@ -4,14 +4,50 @@ public class Ejercicio_1 {
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("La siguiente sucesion es ");
+        System.out.print("Ingrese el numero que va multiplicar: ");
         int sucesion = scanner.nextInt();
         int numeros = 0;
         sucesion = numeros;
         if (sucesion == numeros){
             System.out.print(numeros =10);
+            System.out.print(numeros =20);
+            System.out.print(numeros =30);
+            System.out.print(numeros =40);
             System.out.print(numeros =50);
+            System.out.print(numeros =60);
+            System.out.print(numeros =70);
+            System.out.print(numeros =80);
+            System.out.print(numeros =90);
             System.out.print(numeros =100);
+            System.out.print(numeros =110);
+            System.out.print(numeros =120);
+            System.out.print(numeros =130);
+            System.out.print(numeros =140);
+            System.out.print(numeros =150);
+            System.out.print(numeros =160);
+            System.out.print(numeros =170);
+            System.out.print(numeros =180);
+            System.out.print(numeros =190);
+            System.out.print(numeros =200);
+            System.out.print(numeros =210);
+            System.out.print(numeros =220);
+            System.out.print(numeros =230);
+            System.out.print(numeros =240);
+            System.out.print(numeros =250);
+            System.out.print(numeros =260);
+            System.out.print(numeros =270);
+            System.out.print(numeros =280);
+            System.out.print(numeros =290);
+            System.out.print(numeros =300);
+            System.out.print(numeros =310);
+            System.out.print(numeros =320);
+            System.out.print(numeros =330);
+            System.out.print(numeros =500);
+            System.out.print(numeros =500);
+            System.out.print(numeros =500);
+            System.out.print(numeros =500);
+            System.out.print(numeros =500);
+            System.out.print(numeros =500);
             System.out.print(numeros =150);
             System.out.print(numeros =200);
             System.out.print(numeros =250);
@@ -28,7 +64,9 @@ public class Ejercicio_1 {
             System.out.print(numeros =800);
             System.out.print(numeros =950);
             System.out.print(numeros =1000);
-        }
+        } else if (numeros ==10) {
+            System.out.print(" ,");
 
+        }
     }
 }
---
+++ Examen_1P/src/Multplicador.java
@@ -0,0 +1,11 @@
+public class Multplicador {
+    public static void main(String args [ ]){
+
+        for (int i = 10 ; i <= 1000; i += 10){
+            if (i % 10 ==0){
+
+                System.out.print(", "+ i);
+            }
+        }
+    }
+}
