cb135ea298fc2deebdaca41a5fdacb571d6a2881
Josh18042002 <Taurojoca789@gmail.com>
2025-02-04 16:33:56 -0600

    ejercicio 1

--- Examen_1P/src/Ejercicio_1.java
+++ Examen_1P/src/Ejercicio_1.java
@@ -4,19 +4,30 @@ public class Ejercicio_1 {
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("La siguiente sucesion es: ");
+        System.out.print("La siguiente sucesion es ");
         int sucesion = scanner.nextInt();
         int numeros = 0;
-        int sucesion;
         sucesion = numeros;
         if (sucesion == numeros){
-            System.out.print(numeros =1);
-            System.out.print(numeros =3);
-            System.out.print(numeros =5);
-            System.out.print(numeros =7);
-            System.out.print(numeros =9);
-        } else if (sucesion == numeros ) {
-            System.out.print("No es un numero positivo");
+            System.out.print(numeros =10);
+            System.out.print(numeros =50);
+            System.out.print(numeros =100);
+            System.out.print(numeros =150);
+            System.out.print(numeros =200);
+            System.out.print(numeros =250);
+            System.out.print(numeros =300);
+            System.out.print(numeros =350);
+            System.out.print(numeros =400);
+            System.out.print(numeros =450);
+            System.out.print(numeros =500);
+            System.out.print(numeros =550);
+            System.out.print(numeros =600);
+            System.out.print(numeros =650);
+            System.out.print(numeros =700);
+            System.out.print(numeros =750);
+            System.out.print(numeros =800);
+            System.out.print(numeros =950);
+            System.out.print(numeros =1000);
         }
 
     }
